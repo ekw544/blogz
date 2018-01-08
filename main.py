01198a2aec99a33016ff9d8826571c5d3b5042bf
EKW544 <elisawood10@aol.com>
2018-01-07 21:57:13 -0600

    Changed register.html to signup.html, made a home page, fixed functionalities/ use cases, created dynamic user pages

--- main.py
+++ main.py
@@ -21,82 +21,112 @@ class Blog(db.Model):
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
+    username = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
-    # blogs = db.relationship('Blog', backref='owner')
+    blogs = db.relationship('Blog', backref='owner')
 
-    def __init__(self, email, password):
-        self.email = email
+    def __init__(self, username, password):
+        self.username = username
         self.password = password
 
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'register']
-    if request.endpoint not in allowed_routes and 'email' not in session:
+    allowed_routes = ['login', 'signup', 'list_blogs', 'index']
+    if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect ('/login')
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(username=username).first()
         if user and user.password == password:
-            session['email'] = email
+            session['username'] = username
             flash("Logged in")
-            return redirect('/')
+            return redirect('/newpost')
         else:
             flash('User password incorrect, or user does not exist', 'error')
 
     return render_template('login.html')
 
-@app.route('/register', methods=['POST', 'GET'])
-def register():
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    username_error = ""
+    password_error = ""
+    blanks_error = ""
+    
     if request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
         verify = request.form['verify']
 
-        existing_user = User.query.filter_by(email=email).first()
+        def blanks(field):
+            return field == ''
+
+        def too_short(field):
+            return len(field) < 3
+
+        def mismatch(password, verify):
+            return password != verify
+
+        if blanks(username) == True or blanks(password) == True or blanks(verify) == True:
+            flash('One or more fields were left blank. Please fill out all fields.', 'error')
+            blanks_error = True
+
+        if too_short(username) == True:
+            username_error = 'Username must be at least 3 characters.'
+
+        if too_short(password) == True:
+            password_error = 'Password must be at least 3 characters.'
+
+        if mismatch(password, verify) == True:
+            password_error = 'The password and verify password field entries must match.'
+
+        if username_error != '' or password_error != '' or blanks_error != '':
+            return render_template('signup.html', title='Signup', username=username, password_error=password_error, username_error=username_error)
+
+        existing_user = User.query.filter_by(username=username).first()
         if not existing_user:
-            new_user = User(email, password)
+            new_user = User(username, password)
             db.session.add(new_user)
             db.session.commit()
-            session['email'] = email
-            return redirect('/')
-
+            session['username'] = username
+            return redirect('/newpost')
+        
         else:
-            return "<h1>Duplicate user</h1>"
+            flash('That username already exists. Please choose another.', 'error')
+            return render_template('signup.html', title='Signup', username=username, password_error=password_error, username_error=username_error)
 
-    return render_template('register.html')
+
+    return render_template('signup.html', title='Signup')
 
 @app.route('/logout')
 def logout():
-    del session['email']
+    del session['username']
     flash("Logged Out")
-    return redirect('/')
+    return redirect('/blog')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    owner = User.query.filter_by(email=session['email']).first()
+    users = User.query.all()
 
-    if request.method == 'POST':
-        blog_title = request.form['blog_title']
-        body = request.form['body']
-
-        new_blog = Blog(blog_title, body, owner)
-        db.session.add(new_blog)
-        db.session.commit()
-
-    return render_template('blogs.html', title="Build a Blog!")
+    return render_template('index.html', title="Blog Users!", users=users)
 
 @app.route('/blog')
-def index2():
+def list_blogs():
+    userId = request.args.get('user')
     id = request.args.get('id')
     blogs = Blog.query.all()
+    users = User.query.all()
+
     if id != None:
         blog = find_blog_by_id(id, blogs)
-        return render_template('post.html', title=blog.title, blogs=blog)
+        return render_template('post.html', title=blog.title, post=blog)
+    elif userId != None:
+        blogs_by_user = find_blogs_by_user(userId, blogs)
+        user = find_user_by_id(userId, users)
+        return render_template('blogs.html', title=user.username + "'s Blog", blogs=blogs_by_user)
     else:
         return render_template('blogs.html', title='Blog Posts', blogs=blogs)
 
@@ -109,6 +139,23 @@ def find_blog_by_id(id, blogs):
 
     return result
 
+def find_user_by_id(userId, users):
+    result = None
+    for user in users:
+        if user.id == int(userId):
+            result=user
+            break;
+
+    return result
+
+def find_blogs_by_user(userId, blogs):
+    result = []
+    for blog in blogs:
+        if blog.owner.id == int(userId):
+            result.append(blog)
+
+    return result
+
 @app.route('/newpost')
 def display_newpost_form():
     return render_template('newpost.html', title='New Post')
@@ -118,7 +165,7 @@ def is_empty(entry):
 
 @app.route('/newpost', methods=['POST'])
 def newpost():
-    owner = User.query.filter_by(email=session['email']).first()
+    owner = User.query.filter_by(username=session['username']).first()
     title_error = ""
     content_error = ""
 
